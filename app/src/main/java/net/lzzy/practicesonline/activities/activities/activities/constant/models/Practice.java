23476294ef3e3cfd2831a2d7f7066314fe01a21d
Administrator <1491807832@qq.com>
2019-04-18 19:21:29 +0800

    4月18号 业务逻辑

---
+++ app/src/main/java/net/lzzy/practicesonline/activities/activities/activities/constant/models/Practice.java
@@ -0,0 +1,81 @@
+package net.lzzy.practicesonline.activities.activities.activities.constant.models;
+
+import android.widget.EditText;
+
+import net.lzzy.sqllib.Ignored;
+import net.lzzy.sqllib.Sqlitable;
+
+import java.util.Date;
+
+/**
+ * Created by lzzy_gxy on 2019/4/16.
+ * Description:
+ */
+public class Practice extends BaseEntity implements Sqlitable {
+    @Ignored
+    public static final String COL_NAME = "name";
+    @Ignored
+    public static final String COL_OUTLINES = "outlines";
+    @Ignored
+    public static final String COL_API_ID = "apild";
+    private String name;
+    private int questionCount;
+    private Date downloadDate;
+    private String outlines;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getQuestionCount() {
+        return questionCount;
+    }
+
+    public void setQuestionCount(int questionCount) {
+        this.questionCount = questionCount;
+    }
+
+    public Date getDownloadDate() {
+        return downloadDate;
+    }
+
+    public void setDownloadDate(Date downloadDate) {
+        this.downloadDate = downloadDate;
+    }
+
+    public String getOutlines() {
+        return outlines;
+    }
+
+    public void setOutlines(String outlines) {
+        this.outlines = outlines;
+    }
+
+    public boolean isDownloaded() {
+        return isDownloaded;
+    }
+
+    public void setDownloaded(boolean downloaded) {
+        isDownloaded = downloaded;
+    }
+
+    public int getApild() {
+        return apild;
+    }
+
+    public void setApild(int apild) {
+        this.apild = apild;
+    }
+
+    private boolean isDownloaded;
+    private int apild;
+
+    @Override
+    public boolean needUpdate() {
+        return false;
+    }
+}
